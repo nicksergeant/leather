5b2fc45348552531def051dd109614ac86681b1e
Nick Sergeant <nsergeant@hubspot.com>
2017-07-28 10:13:05 -0400

    Herp.

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :leather, Leather.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "leatherapp.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :logger, level: :info
