7beb27c3f81b8c83cb0aead65362af98bbe814d3
Nick Sergeant <nsergeant@hubspot.com>
2017-08-01 00:30:23 -0400

    Wildcard route for React app.

--- lib/leather_web/router.ex
+++ lib/leather_web/router.ex
@@ -16,7 +16,7 @@ defmodule LeatherWeb.Router do
   scope "/", LeatherWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
