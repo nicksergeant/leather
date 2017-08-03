6b63ddbbb15c9e6bbfbaed5a76ba968e03886beb
Nick Sergeant <nsergeant@hubspot.com>
2017-08-02 23:17:53 -0400

    Minor cleanup.

--- mix.exs
+++ mix.exs
@@ -33,14 +33,15 @@ defmodule Leather.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:cowboy, "~> 1.0"},
+      {:dogma, "~> 0.1", only: :dev},
+      {:gettext, "~> 0.11"},
       {:phoenix, "~> 1.3.0"},
-      {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
