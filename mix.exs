2a21cfad780631e42ac840d12ee4dfbc1326947c
Nick Sergeant <nsergeant@hubspot.com>
2017-08-12 19:21:52 -0400

    Use Elixir 1.5.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Leather.Mixfile do
     [
       app: :leather,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
