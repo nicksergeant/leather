2a9766eba290addea42a5241a9043559f3a988e1
Nick Sergeant <nsergeant@hubspot.com>
2017-09-01 17:30:42 -0400

    exfmt only.

--- mix.exs
+++ mix.exs
@@ -2,64 +2,52 @@ defmodule Leather.Mixfile do
   use Mix.Project
 
   def project do
-    [
-      app: :leather,
-      version: "0.0.1",
-      elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
-      aliases: aliases(),
-      deps: deps()
-    ]
+    [app: :leather,
+     version: "0.0.1",
+     elixir: "~> 1.5",
+     elixirc_paths: elixirc_paths(Mix.env),
+     compilers: [:phoenix, :gettext] ++ Mix.compilers,
+     start_permanent: Mix.env == :prod,
+     aliases: aliases(),
+     deps: deps()]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
+
   def application do
-    [
-      mod: {Leather.Application, []},
-      extra_applications: [:logger, :runtime_tools]
-    ]
+    [mod: {Leather.Application, []},
+     extra_applications: [:logger, :runtime_tools]]
   end
 
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
+  defp elixirc_paths(:test) do
+    ["lib", "test/support"]
+  end
+
+  defp elixirc_paths(_) do
+    ["lib"]
+  end
+
+
   defp deps do
-    [
-      {:bcrypt_elixir, "~> 0.12.1"},
-      {:comeonin, "~> 4.0"},
-      {:cowboy, "~> 1.0"},
-      {:dogma, "~> 0.1", only: :dev},
-      {:exfmt, ">= 0.0.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:phoenix, "~> 1.3.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:phoenix_html, "~> 2.10"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:plaidex, "~> 0.1.0"},
-      {:postgrex, ">= 0.0.0"},
-    ]
+    [{:bcrypt_elixir, "~> 0.12.1"},
+     {:comeonin, "~> 4.0"},
+     {:cowboy, "~> 1.0"},
+     {:dogma, "~> 0.1", [only: :dev]},
+     {:exfmt, ">= 0.0.0", [only: :dev]},
+     {:gettext, "~> 0.11"},
+     {:phoenix, "~> 1.3.0"},
+     {:phoenix_ecto, "~> 3.2"},
+     {:phoenix_html, "~> 2.10"},
+     {:phoenix_live_reload, "~> 1.0", [only: :dev]},
+     {:phoenix_pubsub, "~> 1.0"},
+     {:plaidex, "~> 0.1.0"},
+     {:postgrex, ">= 0.0.0"}]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
+
   defp aliases do
-    [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
-    ]
+    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+     "ecto.reset": ["ecto.drop", "ecto.setup"],
+     test: ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
