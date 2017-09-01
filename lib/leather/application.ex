fb189e9c96f8ab10f97139125af53f639fd85866
Nick Sergeant <nsergeant@hubspot.com>
2017-09-01 18:18:51 -0400

    Handle all errors with sentry in prod.

--- lib/leather/application.ex
+++ lib/leather/application.ex
@@ -1,29 +1,23 @@
 defmodule Leather.Application do
   use Application
 
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
       supervisor(Leather.Repo, []),
-      # Start the endpoint when the application starts
       supervisor(LeatherWeb.Endpoint, []),
-      # Start your own worker by calling: Leather.Worker.start_link(arg1, arg2, arg3)
-      # worker(Leather.Worker, [arg1, arg2, arg3]),
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Leather.Supervisor]
+
+    if Mix.env == :prod do
+      :ok = :error_logger.add_report_handler(Sentry.Logger)
+    end
+
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     LeatherWeb.Endpoint.config_change(changed, removed)
     :ok
