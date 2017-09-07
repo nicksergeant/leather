075362de0db22c37a80450a451f1957753323a47
Nick Sergeant <nsergeant@hubspot.com>
2017-09-07 00:18:31 -0400

    Remove debug stuff.

--- lib/leather_web/channels/account_channel.ex
+++ lib/leather_web/channels/account_channel.ex
@@ -2,14 +2,7 @@ defmodule LeatherWeb.AccountChannel do
   use LeatherWeb, :channel
 
   def join("accounts:" <> account_id, _params, socket) do
-    :timer.send_interval 2000, :ping
+    # TODO: Verify user owns account.
     {:ok, socket}
-  end
-
-
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push socket, "ping", %{count: count}
-    {:noreply, assign(socket, :count, count + 1)}
   end
 end
