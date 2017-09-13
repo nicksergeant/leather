101028a4d4d629a217b2ce31bc54c8858089d0d4
Nick Sergeant <nsergeant@hubspot.com>
2017-09-12 20:01:05 -0400

    Add less support.

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ['static', 'css', 'js', 'vendor'],
+    watched: ['static', 'css', 'js', 'less', 'vendor'],
     // Where to compile files to
     public: '../priv/static',
   },
